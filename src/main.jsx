7307dd5f00d87d38b842b476c9636d5e9f030d73
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-12-21 13:47:08 +0600

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
 import router from './routes/Routes';
+import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
